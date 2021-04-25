9e4bc3cc65de8be7c0585ed111e59bc1083798cd
Janmejay-Joshi <publish.janmejay@gmail.com>
2021-04-25 22:18:34 +0530

    UI Changes

--- app.js
+++ app.js
@@ -1,59 +1,68 @@
+let logo = document.getElementById("logo");
+let summary = document.getElementById("summary");
+let language = "uwu";
+
+document.getElementById("uwu").addEventListener("click", () => {
+    language = "uwu";
+    logo.src = "./assets/images/uwu.jpg";
+    summary.innerHTML =
+        "Uwu is an analytic contour tone language with a very small phonetic inventory. Its native speakers are communes of catgirls who have left behind their physical forms to ascend to a higher plane of existence and live exclusively on the internet.";
+});
+
+document.getElementById("pig-latin").addEventListener("click", () => {
+    language = "pig-latin";
+    logo.src = "./assets/images/pig-latin.png";
+    summary.innerHTML =
+        'Pig Latin, or "Igpay Atinlay" is a language game in which English words are altered, usually by adding a fabricated suffix or by moving the onset or initial consonant or consonant cluster of a word to the end of the word and adding a vocalic syllable to create such a suffix';
+});
+
+document.getElementById("morse").addEventListener("click", () => {
+    language = "morse";
+    logo.src = "./assets/images/morse.png";
+    summary.innerHTML =
+        "Morse code is a method used in telecommunication to encode text characters as standardized sequences of two different signal durations, called dots and dashes or dits and dahs. Morse code is named after Samuel Morse, an inventor of the telegraph";
+});
+
+document.getElementById("english").addEventListener("click", () => {
+    language = "english";
+    logo.src = "./assets/images/upsidedown.jpg";
+    summary.innerHTML =
+        "Australian English (AuE, AuEng) is the set of varieties of the English language native to Australia. Australian English is the country's national and de facto common language.";
+});
+
+document.getElementById("barcode").addEventListener("click", () => {
+    language = "barcode";
+    logo.src = "./assets/images/barcode.jpg";
+    summary.innerHTML =
+        "A barcode or bar code is a method of representing data in a visual, machine-readable form. Initially, barcodes represented data by varying the widths and spacings of parallel lines. ";
+});
+
+document.getElementById("cursive").addEventListener("click", () => {
+    language = "cursive";
+    logo.src = "./assets/images/cursive.jpg";
+    summary.innerHTML =
+        "Cursive is any style of penmanship in which some characters are written joined together in a flowing manner, generally for the purpose of making writing faster, in contrast to block letters. Cursive handwriting is very functional, and is intended to be used in everyday writing.";
+});
+
 [
     document.getElementById("input"),
-    document.getElementById("language"),
+    document.getElementById("uwu"),
+    document.getElementById("cursive"),
+    document.getElementById("english"),
+    document.getElementById("barcode"),
+    document.getElementById("pig-latin"),
+    document.getElementById("morse"),
 ].forEach((element) =>
-    ["input", "focus"].forEach((event) =>
+    ["input", "focus","click"].forEach((event) =>
         element.addEventListener(
             event,
             () =>
                 (document.getElementById("output").value = converter(
-                    document.getElementById("language").value,
+                    language,
                     document.getElementById("input").value
                 ))
         )
     )
-);
-
-["input", "focus"].forEach((event) =>
-    document.getElementById("language").addEventListener(event, () => {
-        switch (document.getElementById("language").value) {
-            case "uwu":
-                document.getElementById("logo").src = "./assets/images/uwu.jpg";
-                document.getElementById("summary").innerHTML =
-                    "Uwu is an analytic contour tone language with a very small phonetic inventory. Its native speakers are communes of catgirls who have left behind their physical forms to ascend to a higher plane of existence and live exclusively on the internet.";
-                break;
-            case "pig-latin":
-                document.getElementById("logo").src =
-                    "./assets/images/pig-latin.png";
-                document.getElementById("summary").innerHTML =
-                    'Pig Latin, or "Igpay Atinlay" is a language game in which English words are altered, usually by adding a fabricated suffix or by moving the onset or initial consonant or consonant cluster of a word to the end of the word and adding a vocalic syllable to create such a suffix';
-                break;
-            case "morse":
-                document.getElementById("logo").src =
-                    "./assets/images/morse.png";
-                document.getElementById("summary").innerHTML =
-                    "Morse code is a method used in telecommunication to encode text characters as standardized sequences of two different signal durations, called dots and dashes or dits and dahs. Morse code is named after Samuel Morse, an inventor of the telegraph";
-                break;
-            case "upsidedown":
-                document.getElementById("logo").src =
-                    "./assets/images/upsidedown.jpg";
-                document.getElementById("summary").innerHTML =
-                    "Australian English (AuE, AuEng) is the set of varieties of the English language native to Australia. Australian English is the country's national and de facto common language.";
-                break;
-            case "barcode":
-                document.getElementById("logo").src =
-                    "./assets/images/barcode.jpg";
-                document.getElementById("summary").innerHTML =
-                    "A barcode or bar code is a method of representing data in a visual, machine-readable form. Initially, barcodes represented data by varying the widths and spacings of parallel lines. ";
-                break;
-            case "cursive":
-                document.getElementById("logo").src =
-                    "./assets/images/cursive.jpg";
-                document.getElementById("summary").innerHTML =
-                    "Cursive is any style of penmanship in which some characters are written joined together in a flowing manner, generally for the purpose of making writing faster, in contrast to block letters. Cursive handwriting is very functional, and is intended to be used in everyday writing.";
-                break;
-        }
-    })
 );
 
 function converter(language, content) {
@@ -74,19 +83,19 @@ function converter(language, content) {
             styler.fontFamily = "Roboto";
             return uwu(newSentence);
             break;
-        case "upsidedown":
+        case "english":
             styler.fontFamily = "UpsideDown";
             return newSentence;
             break;
         case "barcode":
             styler.fontFamily = "BarcodeText";
             styler.fontSize = "50px";
-    styler.fontWeight = "400";
+            styler.fontWeight = "400";
             return newSentence;
             break;
         case "cursive":
             styler.fontFamily = "Ballet";
-    styler.fontWeight = "400";
+            styler.fontWeight = "400";
             return newSentence;
         default:
             return newSentence;
